33b6d487da01c0f1da3d89a7303cc8f26bdb78a5
mathhawk3 <mathhawk3@gmail.com>
2024-03-21 22:03:49 -0700

    Hopefully the bugfix should be included in the built version now lol

--- ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b47188f4a93578d397ee82a908967a385d5299fb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b75cd1809f97d74bfc4760bdbc154c958ad3867f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
